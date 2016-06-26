d6d282306bf07c32830ed5fc74b4af45eca9fbe0
lucid-taylor <taylorpalmer@Taylors-Lucid-Macbook-Pro.local>
2016-06-25 22:38:47 -0500

    Restructured data for filtering

--- js/app.js
+++ js/app.js
@@ -29,225 +29,381 @@ angular.module('uxTools', ['ui.router'])
     });
 })
 
-.controller('prototypingCtrl', function($scope) {
-  $scope.testObjects = 
-    [{ 
+.controller('prototypingCtrl', function($scope, $state) {
+
+  $scope.filters = [
+    {
+      'title':'',
+      'show':true,
+      'canChange':false
+    }, {
+      'title':'Name',
+      'show':true,
+      'canChange':true
+    }, {
+      'title':'Platform',
+      'show':true,
+      'canChange':true
+    }, {
+      'title':'Import',
+      'show':true,
+      'canChange':true
+    }, {
+      'title':'Animations',
+      'show':true,
+      'canChange':true
+    }, {
+      'title':'Navigation',
+      'show':true,
+      'canChange':true,
+    }, {
+      'title':'Transitions',
+      'show':true,
+      'canChange':true
+    }, {
+      'title':'Data',
+      'show':true,
+      'canChange':true
+    }, {
+      'title':'Code',
+      'show':true,
+      'canChange':true
+    }, {
+      'title':'Sharing',
+      'show':true,
+      'canChange':true
+    }
+  ];
+
+  $scope.isFilterVisible = function(string) {
+    for ( var i = 0; i < $scope.filters.length; i++ ) {
+      if ( $scope.filters[i].title === string ) {
+        return $scope.filters[i].show;
+      }
+    }
+  };
+
+  $scope.prototypingTools = [
+    {
       'name':'Framer',
       'url':'http://framerjs.com/',
       'image':'img/prototyping/framer.png',
-      'mac':true,
-      'sketch':true,
-      'photoshop':true,
-      'micro':true,
+      'platforms':
+        {
+          'mac':true
+        },
+      'import':
+        {
+          'photoshop':true,
+          'sketch':true
+        },
       'navigation':true,
       'transition':true,
       'data':true,
       'code':true,
-      'shareWeb':true
-    }, { 
+      'sharing':
+        {
+          'web':true
+        }
+    }, {
       'name':'Principle',
       'url':'http://principleformac.com/',
       'image':'img/prototyping/principle.png',
-      'mac':true,
-      'sketch':true,
+      'platforms':
+        {
+          'mac':true
+        },
+      'import': {
+        'sketch':true
+      },
       'micro':true,
       'navigation':true,
       'transition':true,
-      'shareIos':true,
-      'shareMac':true
-    }, { 
+      'sharing': 
+        {
+          'iOS':true,
+          'mac':true
+        }
+    }, {
       'name':'UX Pin',
       'url':'https://uxpin.com/',
       'image':'img/prototyping/uxpin.png',
-      'web':true,
+      'platforms': {
+        'web':true
+      },
       'design':true,
-      'sketch':true,
-      'photoshop':true,
+      'import': {
+        'sketch':true,
+        'photoshop':true
+      },
       'micro':true,
       'navigation':true,
-      'shareWeb':true
-    }, { 
+      'sharing': 
+        {
+          'web':true
+        }
+    }, {
       'name':'InVision',
       'url':'https://www.invisionapp.com/',
       'image':'img/prototyping/invision.png',
-      'web':true,
-      'sketch':true,
-      'photoshop':true,
+      'platforms': {
+        'web': true
+      },
+      'import': {
+        'sketch':true,
+        'photoshop':true
+      },
       'navigation':true,
       'transition':true,
-      'shareWeb':true,
+      'sharing': {
+        'web':true
+      },
       'fixedHeader':true
-    }, { 
+    }, {
       'name':'Marvel',
       'url':'https://marvelapp.com/',
       'image':'img/prototyping/marvel.png',
-      'web':true,
-      'ios':true,
-      'android':true,
+      'platforms': {
+        'web':true,
+        'iOS':true,
+        'android':true
+      },
       'design':true,
-      'sketch':true,
+      'import': {
+        'sketch':true
+      },
       'navigation':true,
       'transition':true,
-      'shareWeb':true,
-      'shareIos':true,
-      'shareAndroid':true,
-      'shareDesktop':true,
-      'shareAppleWatch':true,
-      'shareAppleTv':true
-    }, { 
+      'sharing': {
+        'web':true,
+        'iOS':true,
+        'android':true,
+        'mac':true,
+        'appleWatch':true,
+        'appleTv':true
+      }
+    }, {
       'name':'Flinto',
       'url':'https://www.flinto.com/mac',
       'image':'img/prototyping/flinto.png',
-      'web':true,
-      'ios':true,
+      'platforms': {
+        'web':true,
+        'iOS':true
+      },
       'micro':true,
       'design':true,
-      'sketch':true,
+      'import': {
+        'sketch':true
+      },
       'navigation':true,
       'transition':true,
-      'shareIos':true,
+      'sharing': {
+        'iOS':true
+      },
       'fixedHeader':true
-    }, { 
+    }, {
       'name':'Flinto Lite',
       'url':'https://www.flinto.com/lite',
       'image':'img/prototyping/flinto-lite.png',
-      'web':true,
-      'ios':true,
+      'platforms': {
+        'web':true,
+        'iOS':true
+      },
       'navigation':true,
       'transition':true,
-      'shareIos':true,
-      'shareAndroid':true,
+      'sharing': {
+        'iOS':true,
+        'android':true
+      },
       'fixedHeader':true
-    }, { 
+    }, {
       'name':'Axure',
       'url':'http://www.axure.com/',
       'image':'img/prototyping/axure.png',
-      'mac':true,
-      'windows':true,
+      'platforms': {
+        'mac':true,
+        'windows':true
+      },
       'design':true,
       'micro':true,
       'navigation':true,
       'transition':true,
-      'shareWeb':true
-    },{ 
-      'name':'Basalmiq',
+      'sharing': {
+        'web':true
+      }
+    }, {
+     'name':'Basalmiq',
       'url':'https://balsamiq.com/',
       'image':'img/prototyping/basalmiq.png',
       'design':true,
       'web':true,
       'navigation':true,
-      'shareWeb':true,
-      'sharePdf':true, // keep an eye out
-    },{ 
+      'sharing': {
+        'web':true,
+        'pdf':true
+      }
+    }, {
       'name':'Lucidchart',
       'url':'https://lucidchart.com/',
       'image':'img/prototyping/lucidchart.png',
       'design':true,
-      'web':true,
+      'platforms': {
+        'web':true
+      },
       'navigation':true,
-      'shareWeb':true
-    },{ 
+      'sharing': {
+        'web':true
+      }
+    }, {
       'name':'Adobe XD',
       'url':'http://www.adobe.com/products/experience-design',
       'image':'img/prototyping/adobe-xd.png',
       'design':true,
-      'mac':true,
+      'platforms': {
+        'mac':true
+      },
       'navigation':true,
       'transition':true,
-      'shareWeb':true
-    }, { 
+      'sharing': {
+        'web':true
+      }
+    }, {
       'name':'Craft',
       'url':'http://www.adobe.com/products/experience-design',
       'image':'img/prototyping/craft.png',
-      'mac':true,
+      'platforms': {
+        'mac':true
+      },
       'sketch':true,
       'navigation':true,
       'transition':true,
-      'shareIos':true
-    },{ 
+      'sharing': {
+        'iOS':true
+      }
+    }, {
       'name':'Pixate',
       'url':'http://www.pixate.com/',
       'image':'img/prototyping/pixate.png',
-      'mac':true,
-      'sketch':true,
+      'platforms': {
+        'mac':true,
+      },
+      'import': {
+        'sketch':true
+      },
       'micro':true,
       'navigation':true,
       'transition':true,
-      'shareIos':true,
-      'shareAndroid':true
-    },{ 
+      'sharing': {
+        'iOS':true,
+        'android':true
+      }
+    }, {
       'name':'Origami',
       'url':'https://facebook.github.io/origami/',
       'image':'img/prototyping/origami.png',
-      'mac':true,
+      'plaforms': {
+        'mac':true,
+      },
       'micro':true,
       'navigation':true,
       'transition':true,
-      'shareIos':true
-    },{ 
+      'sharing': {
+        'iOS':true
+      }
+    }, {
       'name':'Proto.io',
       'url':'https://proto.io/',
       'image':'img/prototyping/proto.io.png',
-      'web':true,
-      'photoshop':true,
+      'platforms': {
+        'web':true,
+      },
+      'import': {
+        'photoshop':true,
+        'dropbox':true,
+      },
       'sketch':true,
       'design':true,
-      'dropbox':true,
       'micro':true,
       'navigation':true,
       'transition':true,
-      'shareIos':true,
-      'shareAndroid':true,
-      'shareWeb':true
-    },{ 
+      'sharing': {
+        'iOS':true,
+        'android':true,
+        'web':true
+      }
+    }, {
       'name':'Moqups',
       'url':'https://moqups.com/',
       'image':'img/prototyping/moqups.png',
-      'web':true,
+      'platforms': {
+        'web':true,
+      },
       'design':true,
       'navigation':true,
-      'shareWeb':true,
-      'sharePdf':true,
-    },{ 
+      'sharing': {
+        'web':true,
+        'pdf':true,
+      }
+    }, {
       'name':'Atomic.io',
       'url':'https://atomic.io/',
       'image':'img/prototyping/atomic.io.png',
-      'web':true,
+      'platforms': {
+        'web':true,
+      },
       'design':true,
       'micro':true,
       'navigation':true,
       'transition':true,
-      'shareWeb':true,
-    },{ 
+      'sharing': {
+        'web':true,
+      }
+    }, {
       'name':'CanvasFlip',
       'url':'http://canvasflip.com/',
       'image':'img/prototyping/canvasflip.png',
-      'web':true,
-      'sketch':true,
-      'photoImport':true, // keep an eye out
+      'platforms': {
+        'web':true,
+      },
+      'import': {
+        'sketch':true,
+        'camera':true
+      },
       'micro':true,
       'navigation':true,
       'transition':true,
-      'shareIos':true,
-      'shareAndroid':true,
-      'shareWeb':true,
-    },{ 
+      'sharing': {
+        'iOS':true,
+        'android':true,
+        'web':true
+      }
+    }, {
       'name':'iRise',
       'url':'https://www.irise.com/',
       'image':'img/prototyping/irise.png',
       'design':true,
-      'web':true,
-      'windows':true,
-      'mac':true,
+      'platforms': {
+        'web':true,
+        'windows':true,
+        'mac':true
+      },
       'micro':true,
       'data':true,
       'navigation':true,
       'transition':true,
-      'shareIos':true,
-      'shareWeb':true,
-      'shareWindows':true,
-      'shareMac':true
-    }];
+      'sharing': {
+        'iOS':true,
+        'web':true,
+        'windows':true,
+        'mac':true
+      }
+    }
+  ];
+
+    // $scope.setFilter = function(filter) {
+    //   filter = !filter;
+    //   $state.reload();
+    // };
 })
 
 .directive('fixedHeaderTable', function($timeout) {
