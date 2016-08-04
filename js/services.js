80cad8f5a7bfa86c21749863f8e5846b7c0fce9b
lucid-taylor <taylorpalmer@Taylors-Lucid-Macbook-Pro.local>
2016-08-04 08:14:47 -0600

    Adding Monitoring Tools

--- js/services.js
+++ js/services.js
@@ -915,13 +915,13 @@ angular.module('uxTools.services', [])
         "platforms": {
             "web": true
         },
+        "playback": true,
         "heatmaps": true,
         "tags": true,
         "location": true,
         "skipInactivity": true,
         "playbackSpeed": true,
-        "sharing": {
-            "web": true,
+        "export": {
             "html": true
         }
     }, {
@@ -931,6 +931,7 @@ angular.module('uxTools.services', [])
         "platforms": {
             "web": true
         },
+        "playback": true,
         "heatmaps": true,
         "tags": true,
         "polls": true,
@@ -939,14 +940,10 @@ angular.module('uxTools.services', [])
         "console": true,
         "skipInactivity": true,
         "playbackSpeed": true,
-        "sharing": {
-            "web": true,
-            "html": true
-        }
         // can watch live sessions
         // can show trending referrers
         // can show trending tweets
-
+        // can see global map of locations
     }, {
         "name": "Fullstory",
         "url": "http://www.fullstory.com/",
@@ -954,17 +951,16 @@ angular.module('uxTools.services', [])
         "platforms": {
             "web": true
         },
+        "playback": true,
         "tags": true,
         "location": true,
         "console": true,
         "skipInactivity": true,
         "playbackSpeed": true,
         "jumpBack": true, // Skip backwards a few seconds
+        "taylorChoice": true
         // Can search according the the html element a user clicked
         // can watch live sessions
-        "sharing": {
-            "web": true,
-        }
     }, {
         "name": "Hotjar",
         "url": "https://www.hotjar.com/",
@@ -972,6 +968,7 @@ angular.module('uxTools.services', [])
         "platforms": {
             "web": true
         },
+        "playback": true,
         "heatmaps": true,
         "location": true,
         "formAnalytics": true,
@@ -979,9 +976,39 @@ angular.module('uxTools.services', [])
         "playbackSpeed": true,
         "skipInactivity": true,
         // host custom surveys like Qualtrics
-        "sharing": {
-            "web": true,
-        }
+    }, {
+        "name": "MouseStats",
+        "url": "https://www.mousestats.com/",
+        "image": "img/monitoring/mousestats.png",
+        "platforms": {
+            "web": true
+        },
+        "playback": true,
+        "heatmaps": true,
+        "formAnalytics": true,
+        "polls": true,
+        "skipInactivity": true,
+    }, {
+        "name": "UserTrack",
+        "url": "https://www.usertrack.net/",
+        "image": "img/monitoring/usertrack.png",
+        "platforms": {
+            "web": true
+        },
+        "playback": true,
+        "heatmaps": true,
+        "location": true
+    }, {
+        "name": "Zeerat",
+        "url": "https://zeerat.com",
+        "image": "img/monitoring/zeerat.png",
+        "platforms": {
+            "web": true
+        },
+        "playback": true,
+        "heatmaps": true,
+        "location": true,
+        "formAnalytics": true,
     }];
 
     var filters = [
@@ -994,6 +1021,61 @@ angular.module('uxTools.services', [])
             "show": true,
             "canChange": true,
             "tipText": "Name of app"
+    }, {
+            "title": "Platform",
+            "show": false,
+            "canChange": true,
+            "tipText": "Platform used to<br> view recordings"
+    }, {
+            "title": "Playback",
+            "show": true,
+            "canChange": true,
+            "tipText": "Records user's<br> actions"
+    }, {
+            "title": "Skip",
+            "show": true,
+            "canChange": true,
+            "tipText": "Can skip inactivity<br> during playback"
+    }, {
+            "title": "Speed",
+            "show": true,
+            "canChange": true,
+            "tipText": "Fast-forward<br> & half-speed"
+    }, {
+            "title": "Jump Back",
+            "show": true,
+            "canChange": true,
+            "tipText": "Go back a few seconds<br> in playback (so useful)"
+    }, {
+            "title": "Console",
+            "show": true,
+            "canChange": true,
+            "tipText": "Records JS console<br> during playback"
+    }, {
+            "title": "Heatmaps",
+            "show": true,
+            "canChange": true,
+            "tipText": "Generates heatmap<br> of actions"
+    }, {
+            "title": "Location",
+            "show": true,
+            "canChange": true,
+            "tipText": "Shows visitor's<br> location"
+    }, {
+            "title": "Form Stats",
+            "show": true,
+            "canChange": true,
+            "tipText": "Analyzes form<br> drop off & stats"
+    }, {
+            "title": "Polls",
+            "show": true,
+            "canChange": true,
+            "tipText": "Can create<br> user surveys"
+    }, {
+            "title": "Export",
+            "show": true,
+            "canChange": true,
+            "tipText": "Format for<br> downloaded"
     }];
 
     return {
