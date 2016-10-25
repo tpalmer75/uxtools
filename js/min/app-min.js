b2b3e2b1f1ff40504e8e9b9f62d162be5bc281cd
lucid-taylor <taylorpalmer@Taylors-Lucid-Macbook-Pro.local>
2016-10-25 07:50:22 -0600

    rFA implementation

--- js/min/app-min.js
+++ js/min/app-min.js
@@ -2846,111 +2846,103 @@ angular.module('uxTools', ['ui.router', 'ngAnimate', 'uxTools.prototyping', 'uxT
               var fromTop = allTh.getBoundingClientRect().top;
               var tableContainer = document.getElementById("table-container");
               var leftCells = document.querySelectorAll(".leftCell");
+              var latestKnownScrollX = 0;
+              var latestKnownScrollY = 0;
+              var ticking = false;
 
-              documentEl.bind("scroll", function() {
-                console.log('scrolling');
+              var onScroll = function() {
+                //console.log("scrolling");
+                latestKnownScrollX = document.body.scrollLeft;
+                latestKnownScrollY = document.body.scrollTop;
+                requestTick();
+              }
 
-                //var allTh = document.querySelectorAll(".th-header");
-                
-                
+              function requestTick() {
+                if (!ticking) {
+                  requestAnimationFrame(update);
+                }
+                ticking = true;
+              }
 
-                documentScrollY = document.body.scrollTop;
-                documentScrollX = document.body.scrollLeft;
-                console.log(documentScrollY);
+              function update() {
+                ticking = false;
+                var currentScrollY = latestKnownScrollY;
+                var currentScrollX = latestKnownScrollX;
 
-                var translateHead = "translate(0,"+ (documentScrollY - fromTop) +"px) translateZ(-1em)";           
+                var translateHead = "translate(0,"+ (currentScrollY - fromTop) +"px) translateZ(-1em)";           
 
-                if (documentScrollY == 0) {
+                if (currentScrollY == 0) {
                   allTh.style.transform = "translate(0, 0)";
                   fromTop = allTh.getBoundingClientRect().top;
-              } else if (documentScrollY > fromTop) {
-                  allTh.style.transform = translateHead;
+                } else if (currentScrollY > fromTop) {
+                    allTh.style.transform = translateHead;
+                }
+
+                var translateCell = "translate("+currentScrollX+"px,0) translateZ(-3em)";
+
+                for( let i=0; i < leftCells.length; i++ ) {
+                  leftCells[i].style.transform = translateCell;
+                  document.getElementById("main-header").style.transform = translateCell;
+                }
+
               }
 
-              var translateCell = "translate("+documentScrollX+"px,0) translateZ(-3em)";
-
-              for( let i=0; i < leftCells.length; i++ ) {
-                leftCells[i].style.transform = translateCell;
-                document.getElementById("main-header").style.transform = translateCell;
-              }
-
-              });
-
+              documentEl.bind("scroll", onScroll);
 
             }, 0);
 
-            
-
-            // documentEl = angular.element(document);
-
-            // var test = element.find('#test');
-            // console.log(test);
-
-            // var allTh = element[0].querySelectorAll(".th-header");
-            // console.log(allTh);
-            // var leftCells = document.querySelectorAll(".leftCell");
-            // console.log(leftCells);
-            // var fromTop = allTh[1].getBoundingClientRect().top;
-
-            // documentEl.bind("scroll",function() {
-
-              // documentScroll = documentEl.scrollTop();
-
-              
-
-              // var translateCell = "translate("+this.scrollLeft+"px,0) translateZ(-3em)";
-              // // I translate every cell from left column one by one
-              // for( let i=0; i < leftCells.length; i++ ) {
-              //   leftCells[i].style.transform = translateCell;
-              // }
-                    
-              // // the same for each header cell
-              // var translateHead = "translate(0,"+ (documentScroll - fromTop) +"px) translateZ(-1em)";
-
-              
-
-            // });
         }
     };
 })
 
-// .directive('fixedHeaderTable', function() {
+// .directive('fixedHeaderTable', function($timeout) {
 //     return {
 //         restrict: 'A',
-//         link: function(scope, element) {
+//         link: function(scope, element, attrs) {
 
 //             console.log('loaded');
 
-//             const allTh = angular.element(document.querySelectorAll(".th-header"));
-//             const leftCells = angular.element(document.querySelectorAll(".leftcell"));
-//             const fromTop = angular.element(allTh[1].getBoundingClientRect().top);
+//             documentEl = angular.element(document);
 
-//             element.addEventListener("scroll",function(){
+//             $timeout(function() {
 
-//               const translateCell = "translate("+this.scrollLeft+"px,0) translateZ(-3em)";
-//               // I translate every cell from left column one by one
+//               var allTh = document.getElementById("test");
+//               var fromTop = allTh.getBoundingClientRect().top;
+//               var tableContainer = document.getElementById("table-container");
+//               var leftCells = document.querySelectorAll(".leftCell");
+
+//               documentEl.bind("scroll", function() {
+//                 console.log('scrolling');
+                
+//                 documentScrollY = document.body.scrollTop;
+//                 documentScrollX = document.body.scrollLeft;
+//                 console.log(documentScrollY);
+
+//                 var translateHead = "translate(0,"+ (documentScrollY - fromTop) +"px) translateZ(-1em)";           
+
+//                 if (documentScrollY == 0) {
+//                   allTh.style.transform = "translate(0, 0)";
+//                   fromTop = allTh.getBoundingClientRect().top;
+//               } else if (documentScrollY > fromTop) {
+//                   allTh.style.transform = translateHead;
+//               }
+
+//               var translateCell = "translate("+documentScrollX+"px,0) translateZ(-3em)";
+
 //               for( let i=0; i < leftCells.length; i++ ) {
 //                 leftCells[i].style.transform = translateCell;
-//               }
-                    
-//               // the same for each header cell
-//               const translateHead = "translate(0,"+ (this.scrollTop - fromTop) +"px) translateZ(-1em)";
-
-//               if (this.scrollTop == 0) {
-//                 for( let i=0; i < allTh.length; i++ ) {
-//                   allTh[i].style.transform = "translate(0, 0)";
-//                 }
-//               } else if (this.scrollTop > fromTop) {
-//                 for( let i=0; i < allTh.length; i++ ) {
-//                   console.log(fromTop, translateHead);
-//                   allTh[i].style.transform = translateHead;
-//                 }
+//                 document.getElementById("main-header").style.transform = translateCell;
 //               }
 
-//             });
+//               });
+
+
+//             }, 0);
+
 //         }
 //     };
 // })
+
 
 // .directive('fixedHeaderTable', function($timeout) {
 //     return {
