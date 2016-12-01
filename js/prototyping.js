61e6a73f1dec92eb4ca4d9fca429bc04dd305b22
lucid-taylor <taylorpalmer@Taylors-Lucid-Macbook-Pro.local>
2016-12-01 09:43:00 -0700

    Framer screens

--- js/prototyping.js
+++ js/prototyping.js
@@ -54,8 +54,8 @@ angular.module('uxTools.prototyping', [])
             "figma": true,
         },
         "micro": true,
-        "navigation": false,
-        "transition": false,
+        "navigation": true,
+        "transition": true,
         "data": true,
         "code": true,
         "sharing": {
